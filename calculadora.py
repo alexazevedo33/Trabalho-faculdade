9431ef9a7e0ad6724c36299cb02be33d92e84f54
alexazevedo33 <alexazevedo33@gmail.com>
2024-05-31 17:00:47 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+a = float(input(' Digite um valor: '))
+b = float(input(' Digite um valor: '))
+soma = a + b
+
+print("Resultado da soma: " + str(soma))
+
+soma = a - b
+print("Resultado da subtração: " + str(soma))
+
+soma = a * b
+print("Resultado da multiplicação: " + str(soma))
+
+soma = a / b
+print("Resultado da divisão: " + str(soma))
